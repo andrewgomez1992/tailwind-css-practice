8eaf2c0bfa368fb0af92519abad656604d7686bf
Drew <drew@Drews-MacBook-Pro.local>
2024-01-22 19:35:40 -0800

    removed hardcoded values for imagecard

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,40 +1,38 @@
 import React from "react";
 
 const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
   return (
-    <div className="container mx-auto">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src={image.webformatURL} alt="" className="w-full" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
-            Photo by John Doe
-          </div>
-          <ul>
-            <li>
-              <strong>Views: </strong>
-              3499
-            </li>
-            <li>
-              <strong>Downloads: </strong>
-              230
-            </li>
-            <li>
-              <strong>Likes: </strong>
-              2300
-            </li>
-          </ul>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image.webformatURL} alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
         </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag1
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        {tags.map((tag) => (
+          <span
+            key={image.id}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
           </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag3
-          </span>
-        </div>
+        ))}
       </div>
     </div>
   );
