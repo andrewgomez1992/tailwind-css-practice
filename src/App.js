b1dcd216340fe86632eda5172ecb3144f31d12ee
Drew <drew@Drews-MacBook-Pro.local>
2024-01-22 18:51:42 -0800

    initial card created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            3499
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            230
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            2300
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
